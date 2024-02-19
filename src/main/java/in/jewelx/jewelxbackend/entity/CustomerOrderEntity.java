c39c1362d3b218af21f2e3bd75ff00c4387a297f
kshitij26 <kshitijburhade9823@gmail.com>
2024-02-19 22:53:16 +0530

    ADD : Added api's to customer order entity

--- src/main/java/in/jewelx/jewelxbackend/entity/CustomerOrderEntity.java
+++ src/main/java/in/jewelx/jewelxbackend/entity/CustomerOrderEntity.java
@@ -1,8 +1,9 @@
 package in.jewelx.jewelxbackend.entity;
 
 import java.math.BigDecimal;
-import java.sql.Date;
+
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,7 +19,11 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "customer_orders")
 public class CustomerOrderEntity {
@@ -32,40 +37,42 @@ public class CustomerOrderEntity {
 	@Column(name = "order_id")
 	private UUID orderId;
 
-	@Column(name = "metal_id", nullable = false)
-	private Long metalId;
+	@ManyToOne
+	@JoinColumn(name = "metal_id", nullable = false)
+	private MetalEntity metal;
 
 	@ManyToOne
 	@JoinColumn(name = "customer_id", nullable = false)
 	private CustomerEntity customer;
 
-	@Column(name = "purity", nullable = false)
-	private Long purity;
+	@Column(name = "purity", precision = 8,scale=3, nullable = false)
+	private BigDecimal purity;
 
 	@Column(name = "article_description", length = 200, nullable = false)
 	private String articleDescription;
 
-	@Column(name = "gross_weight", precision = 8, nullable = false)
+	@Column(name = "gross_weight", precision = 8,scale=3)
 	private BigDecimal grossWeight;
 
-	@Column(name = "net_weight", nullable = false)
-	private Long netWeight;
+	@Column(name = "net_weight", precision = 8,scale=3)
+	private BigDecimal netWeight;
 
-	@Column(name = "paid_amount", precision = 15, nullable = false)
+	@Column(name = "paid_amount", precision = 15)
 	private BigDecimal paidAmount;
 
 	@Column(name = "order_status", length = 2, nullable = false)
 	private String orderStatus;
 
 	@Temporal(TemporalType.DATE)
-	@Column(name = "fulfill_date", nullable = false)
+    @Column(name = "fulfill_date", nullable = false, updatable = false)
 	private Date fulfillDate;
 
 	@Temporal(TemporalType.DATE)
-	@Column(name = "order_date", nullable = false)
+    @Column(name = "order_date", nullable = false)
+    @CreationTimestamp
 	private Date orderDate;
 
-	@Column(name = "metal_rate", precision = 8, nullable = false)
+	@Column(name = "metal_rate", precision = 8,scale=2, nullable = false)
 	private BigDecimal metalRate;
 
 	@CreationTimestamp
@@ -84,9 +91,30 @@ public class CustomerOrderEntity {
 	@JoinColumn(name = "updated_by", nullable = false, updatable = false)
 	private UserEntity updatedBy;
 
+	@ManyToOne
+	@JoinColumn(name = "accounting_id", nullable=false, updatable = false)
+	private AccountingEntity accounting;
+	
 	// Constructors, getters, and setters
 
 	public CustomerOrderEntity() {
-		// Default constructor
+		this.orderId = UUID.randomUUID();
+	}
+
+	public CustomerOrderEntity(MetalEntity metal, CustomerEntity customer, BigDecimal purity, String articleDescription,
+			BigDecimal grossWeight, BigDecimal netWeight, BigDecimal paidAmount, String orderStatus, Date fulfillDate,
+			BigDecimal metalRate ) {
+		super();
+		this.orderId = UUID.randomUUID();
+		this.metal = metal;
+		this.customer = customer;
+		this.purity = purity;
+		this.articleDescription = articleDescription;
+		this.grossWeight = grossWeight;
+		this.netWeight = netWeight;
+		this.paidAmount = paidAmount;
+		this.orderStatus = orderStatus;
+		this.fulfillDate = fulfillDate;
+		this.metalRate = metalRate;
 	}
 }
